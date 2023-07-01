caaaf4dea8a569b4df5b894183f4964c93b74845
Alex <alexkimanthi92@gmail.com>
2023-07-01 18:37:51 +0300

    updated

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private 
+  
+    def anagram?(word)
+      word.downcase.chars.sort == @word.chars.sort && word.downcase != @word
+    end
+  end
+ 
+  
+anagram = Anagram.new("listen")
+words = ["tru", "enlist"]
+binding.pry 
